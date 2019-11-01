870d9cbee25d32009770209a0aa2241a6772abd5
Keerthi Mani <carkeane.getz@gmail.com>
2019-10-31 22:38:12 -0400

    connections are created

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured) VALUES("Double Cheese Burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES("Mushroom & Swiss Double Cheese Burger",0);
+INSERT INTO burgers (burger_name, devoured) VALUES("Grilled Chicken Burger",0);
+INSERT INTO burgers (burger_name, devoured) VALUES("Buttermilk Fried Chicken Burger",0);
+
+
+
